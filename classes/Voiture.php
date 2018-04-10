07ee48da0ee696e9ab7b4999dd663a6398b4ab9f
Rodéric Camesella <roderic.camesella@viacesi.fr>
2018-04-10 09:37:13 +0200

    creation de voiture

---
+++ classes/Voiture.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: black
+ * Date: 10/04/2018
+ * Time: 09:30
+ */
+
+class Voiture
+{
+  public $couleur;
+  public $masse;
+  public $marque;
+  public $puissance;
+
+}
